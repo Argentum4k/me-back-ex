aa97d07b38c1286e1564515256f429211406d823
Argentum4k <torvalds@linux-foundation.org>
2022-10-19 00:07:58 +0300

    pr15 logging

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const auth = require('./middlewares/auth');
 const ErrorHandler = require('./errors/ErrorHandler');
 const { validateNewUser, validateCredentials } = require('./middlewares/celebrations');
 const { NotFoundError } = require('./errors/errors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -18,6 +19,13 @@ app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(cookieParser()); // парсер кук
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 
+app.use(requestLogger); // подключаем логгер запросов
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signin', validateCredentials, login);
 app.post('/signup', validateNewUser, createUser);
 // авторизация
@@ -26,6 +34,7 @@ app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 // ошибки
+app.use(errorLogger); // подключаем логгер ошибок
 app.use('/', (req, res, next) => {
   next(new NotFoundError('Ресурс не найден. Проверьте URL и метод запроса'));
 }); // todo check
